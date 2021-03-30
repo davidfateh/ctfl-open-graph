32923abbffcd475f3a4817b4be1995447a504a4e
David Fateh <davidfateh@gmail.com>
2021-03-30 13:06:42 -0400

    page location preview

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -1,16 +1,56 @@
-import React from 'react';
-import { Paragraph } from '@contentful/forma-36-react-components';
+import React, {useState, useEffect} from 'react';
+import tokens from '@contentful/forma-36-tokens'
+import { TextInput, Note, Button } from '@contentful/forma-36-react-components';
 import { FieldExtensionSDK } from '@contentful/app-sdk';
 
 interface FieldProps {
   sdk: FieldExtensionSDK;
 }
 
+interface BlogPost {
+  sys: {
+    id: string;
+  };
+  fields: {
+    body: object;
+    title: {
+      'en-US': string;
+    };
+  };
+};
+
 const Field = (props: FieldProps) => {
-  // If you only want to extend Contentful's default editing experience
-  // reuse Contentful's editor components
-  // -> https://www.contentful.com/developers/docs/extensibility/field-editors/
-  return <Paragraph>Hello Entry Field Component</Paragraph>;
+  const [title, setTitle] = useState(props.sdk.field.getValue() || null);
+
+  useEffect(() => {
+    props.sdk.window.startAutoResizer();
+
+    props.sdk.entry.fields.content.onValueChanged((entry) => {
+      if (!entry) {
+        setTitle(null);
+        return;
+      }
+
+      props.sdk.space.getEntry<BlogPost>(entry.sys.id).then((data) => {
+        if (data.fields.title['en-US'] !== title) {
+          const title = data.fields.title['en-US'];
+          setTitle(title);
+          props.sdk.field.setValue(title);
+        }
+      });
+    });
+  });
+
+  if (title === null) {
+    return <Note noteType="warning">Link a blog post below to assign a title.</Note>
+  }
+
+  return (
+    <>
+      <TextInput disabled value={title} style={{marginBottom: tokens.spacingM}}/>
+      <Button onClick={() => props.sdk.navigator.openCurrentAppPage({path: `/${props.sdk.entry.getSys().id}`})} buttonType="naked">Preview Open Graph</Button>
+    </>
+  );
 };
 
 export default Field;
--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,13 +1,157 @@
-import React from 'react';
-import { Paragraph } from '@contentful/forma-36-react-components';
+import React, { useState, useEffect } from 'react';
+import {
+    Card,
+    Note,
+    Heading,
+    Paragraph,
+    Typography,
+    TextLink,
+    Button,
+} from '@contentful/forma-36-react-components';
+import tokens from '@contentful/forma-36-tokens';
 import { PageExtensionSDK } from '@contentful/app-sdk';
 
 interface PageProps {
-  sdk: PageExtensionSDK;
+    sdk: PageExtensionSDK;
+}
+
+interface OpenGraphEntry {
+    fields: {
+        title: {
+            'en-US': string;
+        };
+        type: {
+            'en-US': string;
+        };
+        url: {
+            'en-US': string;
+        };
+        image: {
+            'en-US': {
+                sys: {
+                    id: string;
+                };
+            };
+        };
+        content: {
+            'en-US': {
+                sys: {
+                    id: string;
+                };
+            };
+        };
+    };
+    sys: {
+        id: string;
+    };
+}
+
+interface Entry {
+    id: string;
+    title: string;
+    imageUrl: string;
+    previewBody: string;
+    url: string;
+}
+
+interface Content {
+    fields: {
+        body: {
+            'en-US': string;
+        };
+    };
+}
+
+interface Asset {
+    fields: {
+        file: {
+            'en-US': {
+                url: string;
+            };
+        };
+    };
 }
 
 const Page = (props: PageProps) => {
-  return <Paragraph>Hello Page Component</Paragraph>;
+    const [entry, setEntry] = useState<Entry | false>();
+
+    useEffect(() => {
+        // Get the entry ID which is passed in via the URL
+        // @ts-ignore
+        const entryId = props.sdk.parameters?.invocation?.path.replace('/', '');
+
+        // If no entry ID exists, show an error message by setting content to false
+        if (!entryId) {
+            setEntry(false);
+            return;
+        }
+
+        // Get the entry data by getting the linked asset and content body
+        props.sdk.space.getEntry<OpenGraphEntry>(entryId).then((data) => {
+            Promise.all([
+                // Grabs the Image asset of the Open Graph content type
+                props.sdk.space.getAsset<Asset>(
+                    data.fields.image['en-US'].sys.id
+                ),
+                // Grabs the long text body of the blog post
+                props.sdk.space.getEntry<Content>(
+                    data.fields.content['en-US'].sys.id
+                ),
+            ]).then(([asset, content]) => {
+                setEntry({
+                    title: data.fields.title['en-US'],
+                    imageUrl: asset.fields.file['en-US'].url,
+                    previewBody: content.fields.body['en-US'],
+                    url: data.fields.url['en-US'],
+                    id: entryId,
+                });
+            });
+        });
+    }, []);
+
+    if (entry === false) {
+        return <Note noteType="negative">Error retrieving entry!</Note>;
+    }
+
+    if (!entry) {
+        return null;
+    }
+
+    return (
+        <div
+            style={{
+                display: 'flex',
+                width: '100%',
+                height: '100vh',
+                alignItems: 'center',
+                marginTop: tokens.spacingXl,
+                flexDirection: 'column',
+            }}
+        >
+            <div style={{ justifyContent: 'center' }}>
+                <Card>
+                    <Typography>
+                        <Heading>
+                            <TextLink href={entry.url}>{entry.title}</TextLink>
+                        </Heading>
+                        <Paragraph>{entry.previewBody}</Paragraph>
+                    </Typography>
+                    <img
+                        src={entry.imageUrl}
+                        alt=""
+                        style={{ width: '200px' }}
+                    />
+                </Card>
+            </div>
+            <Button
+              buttonType="muted"
+              onClick={() => props.sdk.navigator.openEntry(entry.id)}
+              style={{marginTop: tokens.spacingXl}}
+            >
+                Back to entry
+            </Button>
+        </div>
+    );
 };
 
 export default Page;
